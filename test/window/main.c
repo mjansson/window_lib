5875c9ad880637267a2a0cc75906134e57cd08b7
Mattias Jansson <mattias@rampantpixels.com>
2016-04-21 09:58:32 +0200

    update to latest foundation lib

--- test/window/main.c
+++ test/window/main.c
@@ -21,8 +21,9 @@ test_window_application(void) {
 	memset(&app, 0, sizeof(app));
 	app.name = string_const(STRING_CONST("Window tests"));
 	app.short_name = string_const(STRING_CONST("test_window"));
-	app.config_dir = string_const(STRING_CONST("test_window"));
+	app.company = string_const(STRING_CONST("Rampant Pixels"));
 	app.version = window_module_version();
+	app.exception_handler = test_exception_handler;
 	return app;
 }
 
